aec8f5d868a8e80a7a69aec0c3e88d03544cad5a
Bruno Domingues <brunood07@gmail.com>
2022-09-23 23:28:35 -0300

    feat: generator

---
+++ generators/plopfile.js
@@ -0,0 +1,31 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'creating component',
+
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name:'
+      }
+    ],
+
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.types.ts',
+        templateFile: 'templates/types.ts.hbs'
+      }
+    ]
+  });
+};
